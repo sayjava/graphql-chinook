a86d0e4ca05e12ee4ca16e1ca2ef0f92550bfa29
Raymond Ottun <sayjava@gmail.com>
2020-07-22 19:26:11 +0100

    feat: converted to oecd data

--- src/config.ts
+++ src/config.ts
@@ -1,20 +1,17 @@
 import { generate } from "@skimah/api";
-import { sqlite } from "@skimah/ds-sql";
-import JSONSource from "@skimah/ds-json";
+import CSVSource from "@skimah/ds-csv";
 import { readFileSync } from "fs";
 import { GraphQLSchema } from "graphql";
 
 export default async (): Promise<GraphQLSchema> => {
   const typeDefs = readFileSync("schema.graphql").toString();
-
-  const ratings = new JSONSource({ filepath: "data/ratings.json" });
-  const chinook = sqlite("data/chinook.sqlite");
+  const singleHit = new CSVSource({ filepath: "data/single_hit_forecast.csv" });
 
   const { schema } = await generate({
     typeDefs,
     sources: {
-      ratings,
-      chinook,
+      singleHit,
+      default: singleHit,
     },
   });
 
